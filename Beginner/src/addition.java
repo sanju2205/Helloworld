3494ade86ac7ad68cfc25cfe53aecff201e1dc9e
sanju2205 <sanjeevaniannapareddy22@gmail.com>
2022-11-10 15:52:17 +0530

    eclipse to git

---
+++ Beginner/src/addition.java
@@ -0,0 +1,7 @@
+
+public class addition {
+	int a=50;
+	int b=100;
+	
+
+}
--- Demo/src/demo/Hello.java
+++ Demo/src/demo/Hello.java
@@ -3,6 +3,8 @@ package demo;
 public class Hello {
  public static void main(String[] args) {
 	
-	 System.out.println("helloworld");
+	 int a=50;
+	 int b=20;
+	 System.out.println(a+b);
 }
 }
